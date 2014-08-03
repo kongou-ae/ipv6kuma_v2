93b09f0fe09c1423e4b9461d3eacf80b274c3f0a
kongou-ae <y.matsumoto.ae@gmail.com>
2014-08-03 21:07:24 +0900

    get_fullroute()を実装

--- fullroute-check/fullroute-check.py
+++ fullroute-check/fullroute-check.py
@@ -2,8 +2,35 @@
 # -*- coding:utf-8 -*-
 
 import requests
-from bs4 import BeautifulSoup
-import subprocess
 import re
-import shutil
 import difflib
+
+
+def get_fullroute():
+    '''
+    HEのスナップショットを取得し、ベストパスをファイルに書き込む
+    '''
+    
+    # HEのスナップショットを取得
+    html = requests.get('http://ipv6.he.net/bgpview/bgp-table-snapshot.txt')
+
+    # 取得したスナップショットをファイルに書き込む
+    with open('/opt/ipv6kuma_v2/fullroute-check/bgp-table-snapshot.txt',mode='w',encoding='utf-8') as f:
+        f.write(html.content.decode('utf-8'))
+
+    # 書き込んだファイルを読み込み、正規表現でベストパスを抽出し、ファイルに書き込む
+    regex = re.compile(r'^\*>i([\da-zA-Z]{0,4}:)*\/\d{1,3}')
+    newRoute = open('/opt/ipv6kuma_v2/fullroute-check/new-route.txt',mode='w',encoding='utf-8')
+    with open('/opt/ipv6kuma_v2/fullroute-check/bgp-table-snapshot.txt',mode='r',encoding='utf-8') as f:
+        for line in f:
+            if regex.match(line):
+                newRoute.write(regex.match(line).group() + '\n')
+
+def main():
+    '''
+    メイン処理
+    '''
+    get_fullroute()
+
+if __name__ == '__main__':
+    main()
